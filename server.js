9379b44660e68a98cdc4738aa1ad22322459b621
aldinvm <aldinvm890@gmail.com>
2026-08-20 11:43:30 -0500

    Update server.js

--- server.js
+++ server.js
@@ -87,5 +87,8 @@ app.delete('/api/clases/:id', (req, res) => {
   });
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Servidor activo en puerto ${PORT}`));
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor activo en puerto ${PORT}`);
+});
